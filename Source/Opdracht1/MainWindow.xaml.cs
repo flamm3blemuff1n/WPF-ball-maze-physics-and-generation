c4c1af7924573742cac50248659388f15897a3b8
Steven Impens <s.impens.dev@gmail.com>
2018-04-10 14:33:41 +0200

    Create project, floor and moving camera

---
+++ Source/Opdracht1/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Media3D;
+
+namespace Opdracht1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        public Point3DCollection FloorPoints3D {
+            get {
+                double x = 50.0; // floor width / 2
+                double z = 50.0; // floor length / 2
+                double floorDepth = -2.5; // give the floor some depth so it's not a 2 dimensional plane 
+
+                Point3DCollection points = new Point3DCollection(20);
+                Point3D point;
+                //top of the floor
+                point = new Point3D(-x, 0, z);// Floor Index - 0
+                points.Add(point);
+                point = new Point3D(x, 0, z);// Floor Index - 1
+                points.Add(point);
+                point = new Point3D(x, 0, -z);// Floor Index - 2
+                points.Add(point);
+                point = new Point3D(-x, 0, -z);// Floor Index - 3
+                points.Add(point);
+                //front side
+                point = new Point3D(-x, 0, z);// Floor Index - 4
+                points.Add(point);
+                point = new Point3D(-x, floorDepth, z);// Floor Index - 5
+                points.Add(point);
+                point = new Point3D(x, floorDepth, z);// Floor Index - 6
+                points.Add(point);
+                point = new Point3D(x, 0, z);// Floor Index - 7
+                points.Add(point);
+                //right side
+                point = new Point3D(x, 0, z);// Floor Index - 8
+                points.Add(point);
+                point = new Point3D(x, floorDepth, z);// Floor Index - 9
+                points.Add(point);
+                point = new Point3D(x, floorDepth, -z);// Floor Index - 10
+                points.Add(point);
+                point = new Point3D(x, 0, -z);// Floor Index - 11
+                points.Add(point);
+                //back side
+                point = new Point3D(x, 0, -z);// Floor Index - 12
+                points.Add(point);
+                point = new Point3D(x, floorDepth, -z);// Floor Index - 13
+                points.Add(point);
+                point = new Point3D(-x, floorDepth, -z);// Floor Index - 14
+                points.Add(point);
+                point = new Point3D(-x, 0, -z);// Floor Index - 15
+                points.Add(point);
+                //left side
+                point = new Point3D(-x, 0, -z);// Floor Index - 16
+                points.Add(point);
+                point = new Point3D(-x, floorDepth, -z);// Floor Index - 17
+                points.Add(point);
+                point = new Point3D(-x, floorDepth, z);// Floor Index - 18
+                points.Add(point);
+                point = new Point3D(-x, 0, z);// Floor Index - 19
+                points.Add(point);
+                return points;
+            }
+        }
+
+        public Int32Collection FloorPointsIndices {
+            get {
+                int[] indices = new int[] { 0, 1, 2, 0, 2, 3, 4, 5, 7, 5, 6, 7, 8, 9, 11, 9, 10, 11, 12, 13, 15, 13, 14, 15, 16, 17, 19, 17, 18, 19 };
+                return new Int32Collection(indices);
+            }
+        }
+    }
+}
