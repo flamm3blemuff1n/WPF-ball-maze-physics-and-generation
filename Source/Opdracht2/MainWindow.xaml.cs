dc148edd92077b3b0572fa56791a6d4f65608a88
Steven Impens <s.impens.dev@gmail.com>
2018-04-25 00:19:21 +0200

    Document: added botsing

--- Source/Opdracht2/MainWindow.xaml.cs
+++ Source/Opdracht2/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Timers;
 using System.Collections.Generic;
-using System.Threading;
 using System.Windows;
 using System.Windows.Media.Media3D;
 using System.Windows.Threading;
